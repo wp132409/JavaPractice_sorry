f84be39d4623882e8cfa946a9ce749abffa5dfac
wp132409 <imkimhyunwoo@naver.com>
2015-03-17 15:20:34 +0900

    두번째 수행

---
+++ JavaPractice/src/org/dimigo/basic/Promotion.java
@@ -0,0 +1,23 @@
+package org.dimigo.basic;
+
+public class Promotion {
+
+	public static void main(String[] args) {
+		
+		int salary = 1700000;
+		int clerk = 3;
+		int store = 1500;
+		
+		long yearMoney = (long)salary * 12 * (long)clerk * (long)store;
+		
+		System.out.println("<< 디미베네 연간 인건비 >>");
+		System.out.format("월 평균 급여 : %,d원\n",salary);
+		System.out.format("점포 내 직원 수 : %,d명\n",clerk);
+		System.out.format("점포 수 : %,d개\n",store);
+		
+		System.out.format("\n\n연간 인건비 : %,d원", yearMoney);
+		
+
+	}
+
+}
